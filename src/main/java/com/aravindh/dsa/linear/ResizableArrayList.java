3ec8d83591815070d494125ede9e307ef0b1c41a
aravindhravindran <aravindh.ravindran@gmail.com>
2017-05-25 15:43:28 +0530

    -Adding Implementations for ResizableArrayList

--- src/main/java/com/aravindh/dsa/linear/ResizableArrayList.java
+++ src/main/java/com/aravindh/dsa/linear/ResizableArrayList.java
@@ -18,22 +18,49 @@ public class ResizableArrayList {
     }
 
     public void add(int element){
+        if(size >= items.length){
+            grow();
+        }
+        items[size++] = element;
+    }
 
+    private void grow() {
+        int[] newItems = new int[size*DEFAULT_GROWTH_RATE];
+        for(int i=0; i< items.length; i++){
+            newItems[i] = items[i];
+        }
+        items = newItems;
     }
 
     public int get(int index) throws IndexOutOfBoundsException{
-        return 0;
+        if(!isValidIndex(index)){
+            throw new IndexOutOfBoundsException("Invalid index "+index);
+        }
+        return items[index];
+    }
+
+    private boolean isValidIndex(int index){
+        if(index > size || index < 0){
+            return false;
+        }
+        return true;
     }
 
     public void setAtIndex(int index, int element) throws IndexOutOfBoundsException{
-
+        if(!isValidIndex(index)){
+            throw new IndexOutOfBoundsException("Invalid Index "+index);
+        }
+        items[index] = element;
     }
 
     public boolean isEmpty(){
+        if(size == 0){
+            return true;
+        }
         return false;
     }
 
     public int size(){
-        return 0;
+        return size;
     }
 }
