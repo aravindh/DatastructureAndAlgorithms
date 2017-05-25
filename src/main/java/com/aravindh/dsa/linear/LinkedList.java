f7595c4cd6937a299df65ddcb2aae81906a9f94f
aravindhravindran <aravindh.ravindran@gmail.com>
2017-05-25 16:13:28 +0530

    -Adding method signatures for LinkedList -Adding Testcases for LinkedList

---
+++ src/main/java/com/aravindh/dsa/linear/LinkedList.java
@@ -0,0 +1,43 @@
+package com.aravindh.dsa.linear;
+
+/**
+ * Created by aravindhravindran on 25/5/17.
+ */
+public class LinkedList {
+    private class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public void prepend(int data){
+
+    }
+
+    public void append(int data){
+
+    }
+
+    public void deleteNodeWithValue(int value) throws IllegalArgumentException{
+
+    }
+
+    public Integer middleNodeValue(){
+        return 0;
+    }
+
+    public void reverse(){
+
+    }
+
+    public boolean search(int value){
+        return false;
+    }
+
+}
---
+++ src/test/java/com/aravindh/dsa/linear/LinkedListTest.java
@@ -0,0 +1,67 @@
+package com.aravindh.dsa.linear;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aravindhravindran on 25/5/17.
+ */
+@RunWith(value = JUnit4.class)
+public class LinkedListTest {
+
+    @Test
+    public void prependOnEmptyListSuccessfullyPrependsElement(){
+        LinkedList list = new LinkedList();
+        list.prepend(2);
+        Assert.assertTrue(list.search(2));
+    }
+
+    @Test
+    public void appendOnEmptyListSuccessfullyAppendsElement(){
+        LinkedList list = new LinkedList();
+        list.append(2);
+        Assert.assertTrue(list.search(2));
+    }
+
+    @Test
+    public void deleteNodeWithValueOnNonEmptyListWithValidValueSuccessfullyDeletesElement(){
+        LinkedList list = new LinkedList();
+        list.append(2);
+        list.deleteNodeWithValue(2);
+        Assert.assertFalse(list.search(2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deleteNodeWithValueOnNonEmptyListWithInValidValueThrowsIllegalArgumentException(){
+        LinkedList list = new LinkedList();
+        list.append(2);
+        list.deleteNodeWithValue(3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deleteNodeWithValueOnEmptyListThrowsIllegalArgumentException(){
+        LinkedList list = new LinkedList();
+        list.deleteNodeWithValue(3);
+    }
+
+    @Test
+    public void middleNodeValueOnEmptyListReturnsNull(){
+        LinkedList list = new LinkedList();
+        Assert.assertNull(list.middleNodeValue());
+    }
+
+    @Test
+    public void middleNodeValueOnNonEmptyListReturnsMiddleNodeValue(){
+        LinkedList list = new LinkedList();
+        list.append(1);list.append(2);list.append(3);list.append(4);
+        Assert.assertEquals("With even number of nodes", Integer.valueOf(3), list.middleNodeValue());
+        list.append(5);
+        Assert.assertEquals("With odd number of nodes", Integer.valueOf(3), list.middleNodeValue());
+    }
+
+
+}
