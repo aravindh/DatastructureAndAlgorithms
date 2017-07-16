58648e0b85346b6811ae93ab57c9a84ffbdbed52
aravindhravindran <aravindh.ravindran@gmail.com>
2017-07-16 19:45:13 +0530

    -Adding implementation for insertion sort

--- src/main/java/com/aravindh/dsa/sorting/InsertionSort.java
+++ src/main/java/com/aravindh/dsa/sorting/InsertionSort.java
@@ -9,15 +9,26 @@ public class InsertionSort implements ISort {
     public void sort(List<Integer> elements) {
 
         for(int i = 1; i< elements.size(); i++){
+            ///1.Get element to be inserted
             int current = elements.get(i);
 
-            int j = i-1;
-            while(j >= 0 && elements.get(j) > current){
-                elements.set(j+1, elements.get(j));
-                j--;
+            //2.Identify the element to be placed in sorted block
+            int targetIndex = i-1;
+            while(targetIndex >= 0 && elements.get(targetIndex) > current){
+                elements.set(targetIndex+1, elements.get(targetIndex));
+                targetIndex--;
             }
+            targetIndex++;
+            //3. Move the elements to right to make space
+            moveElementsToRight(elements, targetIndex, i );
+            //4.insert the element
+            elements.set(targetIndex, current);
+        }
+    }
 
-            elements.set(j+1, current);
+    private void moveElementsToRight(List<Integer> elements, int start, int end){
+        for(int j = end -1; j >= start; j--){
+            elements.set(j+1, elements.get(j));
         }
     }
 }
